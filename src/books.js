0b87ce50c6d083d028d4fa5e4655e18e36391fb4
amalia barrigas <amaliabarrigas@gmail.com>
2025-04-25 20:56:08 +0200

    ejercicio realizado hasta el 5

--- src/books.js
+++ src/books.js
@@ -38,15 +38,62 @@
 
 
 // Your code here:
-const booksArray = [];
+const booksArray = [
+  { // Book 1
+ title: "The Old Man and the Sea",
+ pages: 128,
+ author: "Ernest Hemingway",
+ details: {
+ language: "English",
+ description: "One of Hemingway's most famous works, it tells the story of Santiago...",
+}
+  },
+
+
+{ // Book 2
+ title: "The Airbnb Story",
+ pages: 256,
+ author: "Leight Gallagher",
+ details: {
+ language: "English",
+ description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...",
+}
+  }, 
+
+
+{ // Book 3
+  title: "Educated - A Memoir",
+  pages: 352,
+  author: "Tara Westover",
+  details: {
+  language: "English",
+  description: "Educated is an account of the struggle for self-invention...",
+}
+   },
+
+
+{ // Book 4
+  title: "The Art of Learning",
+  pages: 288,
+  author: "Josh Waitzkin",
+  details: {
+  language: "English",
+  description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure."
+
+}
+  },
+
+];
+
+
 
 
 
 
 // Iteration 2 | Book Details
-function getBookDetails() {
+function getBookDetails(bookObject) {
   // Your code here:
-
+return `${bookObject.title} - ${bookObject.author} - ${bookObject.pages} pages`;
 }
 
 
@@ -54,14 +101,18 @@ function getBookDetails() {
 // Iteration 3 | Delete Language
 // Your code here:
 
-
+for ( let i = 0; i< booksArray.length; i++) {
+  delete booksArray[i].details.language;
+}
 
 
 // Iteration 4 | Estimated Reading Time
 // Your code here:
 
-
-
+for ( let i = 0; i < booksArray.length; i++) {
+  const tiempoEstimadoDeLectura = Math.ceil((booksArray[i].pages* 500) /90);
+  booksArray[i].readingTime = tiempoEstimadoDeLectura;
+}
 
 // Bonus: Iteration 5 | Books Dictionary
 
@@ -86,9 +137,21 @@ const dictionary = {
     ],
 };
 
-function booksByAuthor() {
+function booksByAuthor(diccionario) {
   // Your code here:
-  
+  let arrayDeLibros = []
+  for ( let author in diccionario){
+    for ( let i= 0; i< diccionario[author].length; i++){
+      let libro = { 
+        title: diccionario[author][i][0],
+        pages: diccionario[author][i][1],
+        author: author,
+      }
+      arrayDeLibros.push(libro);
+    }
+    
+  }
+  return arrayDeLibros;
 }
 
 
